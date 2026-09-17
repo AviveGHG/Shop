bc5fb33531590b299cb3a31688e693dc1203fa74
NachzahlenGG <jonasjanik2023@gmail.com>
2026-09-17 15:47:27 +0200

    Create save_overrides.php

---
+++ admin/save_overrides.php
@@ -0,0 +1,10 @@
+<?php
+$data = file_get_contents('php://input');
+if ($data) {
+    file_put_contents('../overrides.json', $data);
+    echo json_encode(["success" => true]);
+} else {
+    http_response_code(400);
+    echo json_encode(["error" => "No data received"]);
+}
+?>
